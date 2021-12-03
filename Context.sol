7407d6a14b77807b17304b12b2bc6754fdec97b1
Eugene Fitzherbert <90056738+Eugene0920@users.noreply.github.com>
2021-12-03 16:11:23 +0900

    Create Context.sol

---
+++ Context.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.6.0;
+
+contract Context {
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; 
+        return msg.data;
+    }
+}
