e0cf1eb1be62712cc962f4affbc898a95494ab6f
Eugene Fitzherbert <90056738+Eugene0920@users.noreply.github.com>
2021-12-03 15:54:27 +0900

    Create Counters.sol

---
+++ Counters.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.6.0;
+
+library Counters {
+    using SafeMath for uint256;
+
+    struct Counter {
+        uint256 _value; 
+    }
+
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
+
+    function increment(Counter storage counter) internal {
+        counter._value += 1;
+    }
+
+    function decrement(Counter storage counter) internal {
+        counter._value = counter._value.sub(1);
+    }
+}
