99833b02fcce0c20d6192a2b666e52b871b8fa42
Eugene Fitzherbert <90056738+Eugene0920@users.noreply.github.com>
2021-12-03 16:27:59 +0900

    Create ERC165.sol

---
+++ ERC165.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.6.0;
+import "./IERC165.sol";
+
+contract ERC165 is IERC165 {
+    
+    bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+
+    mapping(bytes4 => bool) private _supportedInterfaces;
+
+    constructor () internal {
+        _registerInterface(_INTERFACE_ID_ERC165);
+    }
+
+    function supportsInterface(bytes4 interfaceId) external view override returns (bool) {
+        return _supportedInterfaces[interfaceId];
+    }
+
+    function _registerInterface(bytes4 interfaceId) internal {
+        require(interfaceId != 0xffffffff, "ERC165: invalid interface id");
+        _supportedInterfaces[interfaceId] = true;
+    }
+}
